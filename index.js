0e287f5bf16238b5381e1ffe2c39b90bbcfd6cd1
Aditi Khedkar <36340264+AditiKhedkar@users.noreply.github.com>
2025-09-02 10:52:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import UrlShortener from "./UrlShortener";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <UrlShortener />
+  </React.StrictMode>
+);
